4cc7b3194ab6551b18a6567beedefc3e8b61fced
faizalhussain314 <faizalhusssain314@gmail.com>
2024-05-28 17:17:12 +0530

    reverted changes to (95ae8d1b87ec6ca1ea334d05a10d1a70bf2605cc)

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { createServer, Server as HTTPServer } from "http";
 import cors from "cors";
-import express, { Request, Response, NextFunction } from "express";
+import express from "express";
 import dotenv from "dotenv";
 import { webRouter } from "routers";
 import { json } from "body-parser";
-import helmet from "helmet";
 
 /**
  * The server class that orchestrates the entire application together.
@@ -39,32 +38,9 @@ export default class Server {
     dotenv.config();
     this.application.use(json());
     this.application.use(cors({ origin: "*" }));
-    // this.application.use((req: Request, res: Response, next: NextFunction) => {
-    //   //   res.setHeader(
-    //   //     "X-Frame-Options",
-    //   //     "ALLOW-FROM https://your-frontend-domain.com"
-    //   //   );
-    //   //   res.setHeader(
-    //   //     "Content-Security-Policy",
-    //   //     "frame-ancestors 'self' https://ai-builder-backend.onrender.com http://localhost:5174 http://localhost:5173"
-    //   //   );
-    //   next();
-    // });
 
     this.application.use(express.static("./static", { index: false }));
     this.application.use("", webRouter);
-
-    // this.application.use(
-    //   helmet({
-    //     contentSecurityPolicy: {
-    //       directives: {
-    //         defaultSrc: ["'self'"],
-    //         frameAncestors: ["*"],
-    //       },
-    //     },
-    //     frameguard: false, // Disable frameguard since we set X-Frame-Options manually
-    //   })
-    // );
   }
 
   /**
